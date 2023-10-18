5f6b0f198a36518857ddf67d6f0f84665fc19a12
DustinFader <dustinfader16@gmail.com>
2023-10-17 21:24:47 -0300

    input is now set up and exit can happen with ctr + c

---
+++ input.js
@@ -0,0 +1,21 @@
+const { stdin } = require("process");
+// setup interface to handle user input from stdin
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  return stdin;
+};
+
+const handleUserInput = function (key) {
+  if (key === '\u0003') {
+    console.log("bye");
+    process.exit();
+  }
+}
+
+stdin.on("data", handleUserInput);
+
+module.exports = {setupInput}
--- play.js
+++ play.js
@@ -1,25 +1,7 @@
-const { stdin } = require("process");
 const { connect } = require("./client");
+const { setupInput, handleUserInput } = require("./input");
 
 console.log("Connecting ...");
 connect();
-
-// setup interface to handle user input from stdin
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  return stdin;
-};
-
-const handleUserInput = function (key) {
-  if (key === '\u0003') {
-    console.log("bye");
-    process.exit();
-  }
-}
-stdin.on("data", handleUserInput);
 
 setupInput();
