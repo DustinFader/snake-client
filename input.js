f8ceb2d433009d90a6bd4bddcce077da0aa6eed6
DustinFader <dustinfader16@gmail.com>
2023-10-17 22:44:19 -0300

    added premaid messages and emoticons

--- input.js
+++ input.js
@@ -15,14 +15,53 @@ const handleUserInput = function (key) {
   if (key === '\u0003') {
     console.log("bye");
     process.exit();
-  } if (key === "w") {
+  }
+
+  if (key === "w") {
     connection.write("Move: up");
-  }  if (key === "a") {
+  }
+
+  if (key === "a") {
     connection.write("Move: left")
-  }  if (key === "s") {
+  }
+
+  if (key === "s") {
     connection.write("Move: down")
-  }  if (key === "d") {
+  }
+  
+  if (key === "d") {
     connection.write("Move: right")
+  }
+
+  if (key === "!") {
+    connection.write("Say: Yahooooo!")
+  }
+
+  if (key === "Q") {
+    connection.write("Say: Waaahhhh!")
+  }
+
+  if (key === "U") {
+    connection.write("Say: Holy bonkers!")
+  }
+
+  if (key === "f") {
+    connection.write("Say: .....boop.....")
+  }
+  if (key === "1") {
+    connection.write("Say: (ง ͠ ᵒ̌ Дᵒ̌ )▬▬ι═══════ﺤ")
+  }
+  if (key === "2") {
+    connection.write("Say: ƪ(“╰ _ ╯ )ʃ")
+  }
+  if (key === "3") {
+    connection.write("Say: ಠ෴ಠ")
+  }
+  if (key === "4") {
+    connection.write("Say: (ᴗᵔᴥᵔ)")
+  }
+  if (key === "5") {
+    connection.write("Say: ≖‿≖")
   }
 }
 
