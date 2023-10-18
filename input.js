3311f310f54e5301d6da821f3b3e21502099a19d
DustinFader <dustinfader16@gmail.com>
2023-10-17 22:13:42 -0300

    client moves the snake on the server with wasd now for directional movement.

--- input.js
+++ input.js
@@ -1,7 +1,9 @@
 const { stdin } = require("process");
 // setup interface to handle user input from stdin
+let connection;
 
-const setupInput = function () {
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -13,9 +15,18 @@ const handleUserInput = function (key) {
   if (key === '\u0003') {
     console.log("bye");
     process.exit();
+  } if (key === "w") {
+    connection.write("Move: up");
+  }  if (key === "a") {
+    connection.write("Move: left")
+  }  if (key === "s") {
+    connection.write("Move: down")
+  }  if (key === "d") {
+    connection.write("Move: right")
   }
 }
 
 stdin.on("data", handleUserInput);
+
 
 module.exports = {setupInput}
--- play.js
+++ play.js
@@ -2,6 +2,5 @@ const { connect } = require("./client");
 const { setupInput, handleUserInput } = require("./input");
 
 console.log("Connecting ...");
-connect();
 
-setupInput();
+setupInput(connect());
